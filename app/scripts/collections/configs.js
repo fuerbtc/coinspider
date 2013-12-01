230311f7b5c185850acbdc1ad1c9f4f51e2299c4
Philippe Mindugui <fuerbtc@gmx.de>
2013-12-01 15:52:54 +0100

    Refactorizacion Config Collection

--- app/scripts/collections/configs.js
+++ app/scripts/collections/configs.js
@@ -4,7 +4,7 @@
 define(['backbone','utils/environment','models/config','localStorage'],function(Backbone,Environment,ConfigClass){
     var Configs = Backbone.Collection.extend({
         model: ConfigClass,
-        localStorage: new Backbone.LocalStorage(Environment.STORAGE_CONFIG);
+        localStorage: new Backbone.LocalStorage(Environment.STORAGE_CONFIG)
     });
 
     return Configs;
